3dcdae9ed7c23ade42730b568d4d1d73d73b917d
rubyaice <andrex.silva.777@gmail.com>
2019-05-05 23:09:12 -0430

    Cambios en suscribe
    Cambios en suscribe

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { NavController } from '@ionic/angular';
+import { Http, Headers, RequestOptions } from '@angular/http';
 
 @Component({
   selector: 'app-home',
@@ -9,11 +10,25 @@ import { NavController } from '@ionic/angular';
 })
 export class HomePage {
 
-  constructor(private router: Router, private navCtrl: NavController) {
+  constructor(private router: Router, private navCtrl: NavController, private http: Http) {
 
   }
 
   login_page() {
     this.router.navigate(['/login']);
+  }
+
+  traerGetRequest() {
+    let headers = new Headers();
+    headers.append("Accept", 'application/json');
+    headers.append('Content-Type', 'application/json' );
+    const requestOptions = new RequestOptions({ headers: headers });
+
+    this.http.post('http://127.0.0.1:3000/customers', requestOptions)
+      .subscribe(data => {
+        console.log(data['_body']);
+       }, error => {
+        console.log(error);
+      });
   }
 }
